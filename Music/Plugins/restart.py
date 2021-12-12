1f57c15543f553775ef4ec378016bd327754b5f6
AbhimanyuRana <91766052+AbhumanyuXMusic@users.noreply.github.com>
2021-12-12 22:23:10 +0530

    Update restart.py

--- Music/Plugins/restart.py
+++ Music/Plugins/restart.py
@@ -27,7 +27,7 @@ async def theme_func(_, message):
        pass
     for x in served_chats:
         try:
-            await app.send_message(x, "Music has just restarted herself. Sorry for the issues.\n\nStart playing after 10-15 seconds again.")
+            await app.send_message(x, "Alisha has just restarted herself. Sorry for the issues.\n\nStart playing after 10-15 seconds again.")
         except Exception:
             pass
     served_chatss = []
@@ -43,7 +43,7 @@ async def theme_func(_, message):
         except Exception as e:
             await message.reply_text(f"{e}")
             pass    
-    x = await message.reply_text(f"__Restarting MentosMusic!__")   
+    x = await message.reply_text(f"__Restarting AlishaMusic!__")   
     await start_restart_stage(x.chat.id, x.message_id)
     os.execvp(f"python{str(pyver.split(' ')[0])[:3]}", [
               f"python{str(pyver.split(' ')[0])[:3]}", "-m", "Music"])
